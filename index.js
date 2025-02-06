4ea718b172d466c12e47967f54cfe4632c838e05
Alan Nuril <alannuril013@gmail.com>
2025-02-06 18:29:36 +0700

    make a gradient element on a hero image

---
+++ index.js
@@ -0,0 +1,9 @@
+<script>
+    // Ambil elemen tombol
+    const heroButton = document.querySelector('.hero-btn');
+
+    // Tambahkan event listener untuk klik
+    heroButton.addEventListener('click', function() {
+        alert('Tombol diklik!')
+    });
+</script>
